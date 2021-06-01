0f04c6d1c15f62a99179aefd98e2136c6ef43cd6
Hamza <hamza.seghir@protonmail.com>
2021-06-01 16:59:30 +0200

    redirect to authorization

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ async function generateCodeChallenge(codeVerifier){
 
 const codeVerifier = generateRandomString(128);
 const codeChallenge = generateCodeChallenge(codeVerifier);
-const clientId = "92ade57dd3514375bcd6990adc441c4a";
-const redirectUri = "http://localhost:5501/callback";
+const clientId = "92ade57dd3514375bcd6990adc441c4a"; // https://developer.spotify.com/ code fournit lors de la création de l'app
+const redirectUri = encodeURI("http://localhost:5501/callback"); // https://developer.spotify.com/dashboard/applications à définir dans le dashboard
 
 
 var authorizationUri = `https://accounts.spotify.com/authorize?client_id=${clientId}
-                          &reponse_type=code&redirect_uri=${redirectUri}&code_challenge_method=256&code_challenge=${codeChallenge}`;
+                          &response_type=token&redirect_uri=${redirectUri}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
+
+var logging = document.querySelector("#loggin button");
+
+logging.addEventListener('click', () => window.location=authorizationUri);
