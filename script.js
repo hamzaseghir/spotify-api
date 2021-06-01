d91af5d68e81998c42e381601d51489b19b58fcf
Hamza <hamza.seghir@protonmail.com>
2021-06-01 16:30:18 +0200

    added authorization uri

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ async function generateCodeChallenge(codeVerifier){
     return res;
 }
 
-  
-  const codeVerifier = generateRandomString(128);
-  const codeChallenge = generateCodeChallenge(codeVerifier);
- 
-  console.log(codeChallenge);
+const codeVerifier = generateRandomString(128);
+const codeChallenge = generateCodeChallenge(codeVerifier);
+const clientId = "92ade57dd3514375bcd6990adc441c4a";
+const redirectUri = "http://localhost:5501/callback";
+
+
+var authorizationUri = `https://accounts.spotify.com/authorize?client_id=${clientId}
+                          &reponse_type=code&redirect_uri=${redirectUri}&code_challenge_method=256&code_challenge=${codeChallenge}`;
