c23c57552efb165b079852ea9fde2711fa519411
Hamza <hamza.seghir@protonmail.com>
2021-06-01 17:19:55 +0200

    working authorization and callback, access token in parameter

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ async function generateCodeChallenge(codeVerifier){
     const hash = await crypto.subtle.digest('SHA-256', data);
     const res = btoa(hash);
     return res;
-}
+};
 
+async function generateUri(codeChallenge){
+    var authorizationUri = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
+    return authorizationUri;
+};
+
+const clientId = "92ade57dd3514375bcd6990adc441c4a"; // https://developer.spotify.com/ code fournit lors de la création de l'app
+const redirectUri = encodeURI("http://localhost:5501/"); // https://developer.spotify.com/dashboard/applications à définir dans le dashboard
 const codeVerifier = generateRandomString(128);
 const codeChallenge = generateCodeChallenge(codeVerifier);
-const clientId = "92ade57dd3514375bcd6990adc441c4a"; // https://developer.spotify.com/ code fournit lors de la création de l'app
-const redirectUri = encodeURI("http://localhost:5501/callback"); // https://developer.spotify.com/dashboard/applications à définir dans le dashboard
 
-
-var authorizationUri = `https://accounts.spotify.com/authorize?client_id=${clientId}
-                          &response_type=token&redirect_uri=${redirectUri}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
 
 var logging = document.querySelector("#loggin button");
 
-logging.addEventListener('click', () => window.location=authorizationUri);
+
+
+logging.addEventListener('click', async() => {
+    var authorizationUri = await codeChallenge.then(e => generateUri(e));
+    window.location=authorizationUri;
+    
+});
