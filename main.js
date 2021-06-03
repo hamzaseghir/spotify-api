dd3788697187e1b32b3c45e87bb8ffecdcd1215b
Hamza <hamza.seghir@protonmail.com>
2021-06-03 09:13:06 +0200

    beaucoup de truc

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const access_token = sessionStorage.getItem("acces_token");
 const addButtons = document.querySelectorAll('#results button');
 const ulPlaylist = document.querySelector("#playlist ul");
 const searchBtn = document.querySelector("#search button");
+const searchBar = document.querySelector("#search-input");
+const result = document.querySelector("#results");
+const resultSearch = document.querySelector("#results ul");
+const playlistResult = document.querySelector("#playlist ul")
+const saveButton = document.querySelector("#playlist button");
+var searchQuery = null;
+
 
 console.log(searchBtn);
 console.log(access_token)
@@ -13,7 +20,6 @@ addButtons.forEach(button => {
         const liText = document.createTextNode(`${button.dataset.artist} - ${button.dataset.song}`);
         li.append(liText);
         const deleteButton = document.createElement('button');
-
         deleteButton.addEventListener('click', () => {
             li.remove();
         })
@@ -24,7 +30,7 @@ addButtons.forEach(button => {
     })
 });
 
-const searchSong = () => {
+function searchSong () {
     const options = {
         method: "GET",
         headers : {   
@@ -33,9 +39,73 @@ const searchSong = () => {
             Authorization: `Bearer ${access_token}`,
         }
     }
-    fetch("https://api.spotify.com/v1/search?q=muse&type=track", options)
-    .then(reponse => reponse.json())
-    .then(data => console.log(data));
+    searchQuery = searchBar.value;
+    
+    return fetch(`https://api.spotify.com/v1/search?q=${searchQuery}&type=track`, options);
 }
 
-searchBtn.addEventListener('click', searchSong);
+searchBtn.addEventListener('click', async() => { 
+    const resultLi = document.querySelectorAll("#results li");
+    if(resultLi != null)
+    resultLi.forEach(e => e.remove());
+    const res = await Promise.resolve(searchSong().then(e => e.json()));
+    const items = res.tracks.items;
+    console.log(items);
+    items.forEach(function(e){
+        const uri = e.uri;
+        const titre = e.name;
+        var artist = "";
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.innerText = " # Ajouter";
+        e.artists.forEach(e => artist+= e.name + " ");
+        var liCont = `${titre} - ${artist}`;
+        li.setAttribute("data-uri", uri);
+        li.append(liCont);
+        li.append(button);
+        button.addEventListener('click', addToPlayList(li));
+        resultSearch.append(li);
+    });
+    result.style.display = "block";
+});
+
+function addToPlayList(track){
+    playlistResult.append(track);
+    console.log(track);
+}
+
+function getUser(){
+    const options = {
+        method : "GET",
+        headers: {
+            Accept : "application/json",
+            "Content-Type": "application/json",
+            Authorization : `Bearer ${access_token}`,
+        }
+    }
+    return fetch('https://api.spotify.com/v1/me', options).then(e => e.json());
+}
+
+getUser().then(e => console.log(e));
+
+function createPlaylist(name){
+    const options = {
+        method = "POST",
+        data : {
+            "name" : name,
+            "description" : "Generated with an awesome website",
+            "public" : true,
+        },
+        headers : {
+            Accept : "application/json",
+            "Content-Type" : "application/json",
+            Authorization : `Bearer ${access_token}`
+        }
+    }
+    const userId = getUser().id;
+    return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`).then(e => e.json());
+}
+
+function addTracksToPlaylist(tracks){
+    
+}
